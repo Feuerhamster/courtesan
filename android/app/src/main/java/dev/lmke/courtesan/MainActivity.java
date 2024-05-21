421cb977be6f9a48f926e88fc69e2479e9171111
Feuerhamster <38376566+Feuerhamster@users.noreply.github.com>
2024-05-21 19:13:13 +0200

    first commit

---
+++ android/app/src/main/java/dev/lmke/courtesan/MainActivity.java
@@ -0,0 +1,5 @@
+package dev.lmke.courtesan;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+	appId: "dev.lmke.courtesan",
+	appName: "Courtesan",
+	webDir: "build",
+	bundledWebRuntime: false
+};
+
+export default config;
